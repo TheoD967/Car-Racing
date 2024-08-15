e757601728c4d06cc74123f2ec83ad0d485b94ef
TheoD967 <20035@wc.school.nz>
2024-08-16 11:10:07 +1200

    Adder Barrier Manager system

--- Car - Racing/Assets/Scripts/BarrierManager.cs
+++ Car - Racing/Assets/Scripts/BarrierManager.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackMenuManager : MonoBehaviour
+public class BarrierManager : MonoBehaviour
 {
-    public static int track;
+    public List<GameObject> barriers = new List<GameObject>();
+
+    private int[] trackOne = { 0, 1, 2, 3, 4, 5 };
+    private int[] trackTwo = { 0, 1, 6, 7, 4, 5 };
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
@@ -16,10 +18,5 @@ public class TrackMenuManager : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void SelectTrack(int trackId)
-    {
-        track = trackId;
     }
 }
--- Car - Racing/Assets/Scripts/TrackMenuManager.cs
+++ Car - Racing/Assets/Scripts/TrackMenuManager.cs
@@ -1,25 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TrackMenuManager : MonoBehaviour
 {
+    public List<GameObject> tracks = new List<GameObject>();
+
     public static int track;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SelectTrack(int trackId)
     {
-
+        track = trackId;
+        SceneManager.LoadScene("Race Track");
     }
 }
