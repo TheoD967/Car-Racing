1bb9f101bebd72abafca0a1137b101cd0a5f33fa
TheoD967 <20035@wc.school.nz>
2024-08-21 12:30:02 +1200

    Made barrier manager so diff barriers when diff  track selected

--- Car - Racing/Assets/Scripts/BarrierManager.cs
+++ Car - Racing/Assets/Scripts/BarrierManager.cs
@@ -7,10 +7,27 @@ public class BarrierManager : MonoBehaviour
     public List<GameObject> barriers = new List<GameObject>();
 
     private int[] trackOne = { 0, 1, 2, 3, 4, 5 };
-    private int[] trackTwo = { 0, 1, 6, 7, 4, 5 };
+    private int[] trackTwo = { 0, 1, 6, 7, 5 };
+    private int[] trackBarriers;
 
     // Start is called before the first frame update
     void Start() {
+        if (TrackMenuManager.track == 0)
+        {
+            trackBarriers = trackOne;
+        }
+        if (TrackMenuManager.track == 1)
+        {
+            trackBarriers = trackTwo;
+        }
+        foreach (int item in trackBarriers)
+        {
+            GameObject barrier = barriers[item];
+            barrier.SetActive(true);
+            
+
+        }
+
         
     }
 
